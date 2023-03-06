6b4b9ca00258e98ac8e33b52659d0d23aec23fbf
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-03-06 15:15:38 -0500

    completed lab

--- index.js
+++ index.js
@@ -39,10 +39,35 @@ const createPokemon = (e) => {
 
 pokeForm.addEventListener("submit", createPokemon);
 
+
+// The url in a patch requires an id to identify the resource being updated.
+// The HTTP method is 'PATCH'
+// The body can take the full resource or one property of the resource.
+
+// fetch(url/:id, {
+//   method: 'PATCH',
+//   headers: {
+//       'Content-Type': 'application/json'
+//   },
+//   body:JSON.stringify({prop:value})
+// })
+// ```
 const increaseLikes = (e, char, likeNum) => {
   e.stopPropagation();
   ++char.likes;
-  likeNum.textContent = char.likes;
+  //DOM manipulation
+  //likeNum.textContent = char.likes;
+  fetch(`http://localhost:3000/characters/${char.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+
+    },
+    body: JSON.stringify({likes: char.likes })
+  })
+  //pessimistic will wait for the response to come back then log it 
+  .then((response) => response.json())
+  .then((charData) => likeNum.textContent = charData.likes)
 };
 
 const renderComment = (comment, commentsDiv) => {
@@ -154,7 +179,14 @@ const renderPokemon = (char) => {
   deleteBttn.textContent = "delete";
   deleteBttn.addEventListener("click", (e) => {
     e.stopPropagation();
-    pokeCard.remove();
+    fetch(`http://localhost:3000/characters/${char.id}`, {
+      method: 'DELETE'
+    })
+    //pessimistic
+    .then((response) => response.json())
+    .then(() => pokeCard.remove())
+    //optimistic
+    //pokeCard.remove();
   });
 
   pokeCard.append(pokeImg, pokeName, pokeLikes, likeNum, likesBttn, deleteBttn);
